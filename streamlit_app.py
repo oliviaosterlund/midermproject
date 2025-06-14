d5320ec72c1d16ba33011647ace0f62fae8ca748
oliviaosterlund <oliviaosterlund@nyu.edu>
2025-06-14 17:26:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -106,9 +106,6 @@ elif page == "Predictions":
     X = df[features_selection]
     y = df["exam_score"]
 
-    st.dataframe(X.head())
-    st.dataframe(y.head())
-
     from sklearn.model_selection import train_test_split
     X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
 
